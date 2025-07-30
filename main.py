c28be143bc220af81db481dd07a5c56e9b256f30
Tristan Day <tmd526@gmail.com>
2025-07-30 13:40:28 -0500

    created verbosity tooling

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -11,12 +12,25 @@ client = genai.Client(api_key=api_key)
 def main():
     print("Hello from ai-agent!")
     if len(sys.argv) > 1:
-        response = client.models.generate_content(model='gemini-2.0-flash-001', contents=str(sys.argv[1]))
-        print(response.text)
-        print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count) + "\n" + "Response tokens: " + str(response.usage_metadata.candidates_token_count))
+        user_prompt = sys.argv[1]
+        messages = [
+            types.Content(role="user", parts=[types.Part(text=user_prompt)])
+        ] 
+
+        if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
+            response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages,)
+            print(response.text)
+            prompt_tokens = str(response.usage_metadata.prompt_token_count)
+            response_tokens = str(response.usage_metadata.candidates_token_count)
+            print(f"""User prompt: {user_prompt}
+                  Prompt tokens: {prompt_tokens}
+                  Response tokens: {response_tokens}""")
+        response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages)
+        print(response.txt) 
     else:
         print("No prompt provided use uv run main.py {prompt}")
-        return 1
+        exit(1)
+    exit(0)
     
 if __name__ == "__main__":
     main()
